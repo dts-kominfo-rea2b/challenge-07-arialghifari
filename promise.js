b99ee264da54f6f612531205d60b1844ccc7d83c
arialghifari <arialghifari99@gmail.com>
2022-06-10 15:44:43 +0800

    [Done] JS Promise Challange

--- promise.js
+++ promise.js
@@ -1,7 +1,25 @@
 const { promiseTheaterIXX, promiseTheaterVGC } = require("./external.js");
 
 // TODO: Buat fungsi promiseOutput sesuai ketentuan readme
-const promiseOutput = null;
+let penontonMarah = 0;
+let penontonTidakMarah = 0;
+
+const promiseOutput = async (emosi) => {
+  const dataTheaterIXX = await promiseTheaterIXX(emosi);
+  const dataTheaterVGC = await promiseTheaterVGC(emosi);
+
+  if (emosi == "marah") {
+    dataTheaterIXX.map((films) => films.hasil == "marah" ? penontonMarah++ : "");
+    dataTheaterVGC.map((films) => films.hasil == "marah" ? penontonMarah++ : "");
+
+    return penontonMarah;
+  } else {
+    dataTheaterIXX.map((films) => films.hasil == "tidak marah" ? penontonTidakMarah++ : "");
+    dataTheaterVGC.map((films) => films.hasil == "tidak marah" ? penontonTidakMarah++ : "");
+
+    return penontonTidakMarah;
+  }
+};
 
 module.exports = {
   promiseOutput,
